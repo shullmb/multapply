4fa4063cfd1a0ada1997c0cfd32a22ce2c43d2f4
ericaschenker <ericaschenker@gmail.com>
2018-07-15 14:34:46 -0700

    installed react-router-dom and added dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">MULTAPPLY</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">MULTAPPLY</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bp = require('body-parser');
+import ReactDOM from 'react-dom';
 const mongoose = require('mongoose');
 const expressJWT = require('express-jwt');
 const auth = require('./routes/auth');
