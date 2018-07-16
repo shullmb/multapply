8a5819c2ecae45ed451ad5fde29cfedded19219a
ericaschenker <ericaschenker@gmail.com>
2018-07-16 11:55:11 -0700

    added in material-ui stuffs

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
+import Grid from '@material-ui/core/Grid';
 
 class Navbar extends Component {
   
   render() {
     return (
       <div>
+        <Grid container spacing={24}>
+        <Grid item xs={6} sm={3}>
         <Login liftTokenToState={ this.props.liftTokenToState} />{` `}
+        </Grid>
+        <Grid item xs={6} sm={3}>
         <Signup liftTokenToState={this.props.liftTokenToState} />
+        </Grid>
+        <Grid item xs={6} sm={3}>
         <a onClick={this.props.logout}>Log out</a>
+        </Grid>
+        </Grid>
       </div>
     )
   }
