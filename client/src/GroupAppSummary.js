8eb7b32bc6f957467f74a1439b00e9fc6f0a4a55
shullmb <shullmb@gmail.com>
2018-07-18 16:52:54 -0700

    added Group Summary modal

--- client/src/GroupAppSummary.js
+++ client/src/GroupAppSummary.js
@@ -1,1 +1,39 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
+import Button from '@material-ui/core/Button'
+
+class GroupAppSummary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+    this.onOpenGroupAppModal = this.onOpenGroupAppModal.bind(this)
+    this.onCloseGroupAppModal = this.onCloseGroupAppModal.bind(this)
+  }
+
+  onOpenGroupAppModal() {
+    this.setState({ open: true });
+  };
+
+  onCloseGroupAppModal() {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    const groupSummary = this.props.group ? this.props.group.map( member => <h3>{member.name} : {member.creditScore}</h3>) : ''
+    return (
+      <div>
+        <Button variant="outlined" color="primary" onClick={this.onOpenGroupAppModal} >
+          <a>See Group Application Summary</a>
+        </Button>
+        <Modal open={open} onClose={this.onCloseGroupAppModal} center>
+          {groupSummary}
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default GroupAppSummary;
