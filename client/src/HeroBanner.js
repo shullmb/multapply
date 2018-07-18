018612262be3c82d45f302884e4123af30ca0ba9
ericaschenker <ericaschenker@gmail.com>
2018-07-18 13:14:42 -0700

    added some styling on landing page

--- client/src/HeroBanner.js
+++ client/src/HeroBanner.js
@@ -7,7 +7,7 @@ class HeroBanner extends Component {
   render() {
     return (
       <div>
-        <Grid container spacing={24}>
+        <Grid container justify='center' alignItems='center' alignContent='center'>
           <Grid item xs={12}>
           <div class="hero-banner">
             <img class="hero-image" src="../img/hero-image.jpg" />
@@ -16,13 +16,13 @@ class HeroBanner extends Component {
             <Grid item xs={6}>
             <img class="rental-pic" src="../img/rental-pic.jpg" />
             </Grid>
-            <Grid item xs={6}>
-              <p>Renters can fill out their standard application and submit it to properties with ease. Roommates can create
+            <Grid xs={6}>
+              <p class="hero-blurb" >Renters can fill out their standard application and submit it to properties with ease. Roommates can create
                 groups and apply together, rather than individually, saving you time and frustration.
               </p>
             </Grid>
             <Grid item xs={6}>
-            <p>Property managers can post listings and view groups of applicants, avoiding the pain 
+            <p class="hero-blurb">Property managers can post listings and view groups of applicants, avoiding the pain 
               of running credit checks and looking over multiple applications. 
             </p>
                 </Grid>
